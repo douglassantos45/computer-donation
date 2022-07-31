f898db6ab6c4d79e011a522528a5bc258d24dd85
Douglas Tott <douglasifbaianosb@gmail.com>
2022-07-31 15:41:42 -0300

    Formulario funcionando versão final

--- src/pages/forms/step2/index.tsx
+++ src/pages/forms/step2/index.tsx
@@ -9,11 +9,32 @@ import styles from './styles.module.scss';
 export default function Step2() {
   const [serviceList, setServiceList] = useState([{ type: '', condition: '' }]);
 
-  const amount = 2;
+  let amount = 2;
+
+  const equipments = [
+    { value: 'notebook', name: 'Notebook' },
+    { value: 'desktop', name: 'Desktop' },
+    { value: 'netBook', name: 'NetBook' },
+    { value: 'screen', name: 'Monitor' },
+    { value: 'printer', name: 'Impressora' },
+    { value: 'scanner', name: 'Scanner' },
+  ];
+
+  const states = [
+    {
+      value: 'working',
+      name: 'Tem todas as partes, liga e funciona normalmente',
+    },
+    { value: 'notWorking', name: 'Tem todas as partes, mas não liga mais' },
+    {
+      value: 'broken',
+      name: ' Faltam peças, funciona só as vezes ou está quebrado',
+    },
+  ];
 
   useEffect(() => {
-    Array.from(Array(2)).map(_ => handleServiceAdd());
-  }, []);
+    Array.from(Array(amount)).map(_ => handleServiceAdd());
+  }, [amount]);
 
   const handleServiceAdd = () => {
     setServiceList([...serviceList, { type: '', condition: '' }]);
@@ -24,7 +45,13 @@ export default function Step2() {
   };
 
   const handleServiceRemove = index => {
-    console.log('');
+    const list = [...serviceList];
+    list.splice(index, 1);
+    setServiceList(list);
+
+    amount -= 1;
+
+    console.log(amount);
   };
 
   const handleServiceChange = (e: ChangeEvent<HTMLSelectElement>, index) => {
@@ -38,27 +65,42 @@ export default function Step2() {
   return (
     <>
       <form action="">
-        <div>
-          <label htmlFor="service">Services</label>
+        <div className={styles.container}>
           {serviceList.map((service, index) => (
-            <div key={index}>
-              <select
-                name="type"
-                value={service.type}
-                onChange={e => handleServiceChange(e, index)}
-              >
-                <option value="">Selecione</option>
-                <option value="notebook">Notebook</option>
-              </select>
+            <div key={index} className={styles.input_group}>
+              <label htmlFor="service">
+                <select
+                  name="type"
+                  value={service.type}
+                  onChange={e => handleServiceChange(e, index)}
+                >
+                  <option value="">Selecione...</option>
+                  {states.map(({ name, value }) => (
+                    <option value={value} key={value}>
+                      {name}
+                    </option>
+                  ))}
+                </select>
+              </label>
 
-              <select
-                name="condition"
-                value={service.condition}
-                onChange={e => handleServiceChange(e, index)}
-              >
-                <option value="">Selecione</option>
-                <option value="ruim">ruim</option>
-              </select>
+              <label>
+                <select
+                  name="condition"
+                  value={service.condition}
+                  onChange={e => handleServiceChange(e, index)}
+                >
+                  <option value="">Selecione</option>
+                  {equipments.map(({ name, value }) => (
+                    <option value={value} key={value}>
+                      {name}
+                    </option>
+                  ))}
+                </select>
+              </label>
+
+              <button onClick={e => handleServiceRemove(index)} type="button">
+                Remover
+              </button>
             </div>
           ))}
         </div>
